2a0bb04da50cf4c5ecfe6e87262cd94afa3ac7d5
balchantus <89977635+balchantus@users.noreply.github.com>
2021-10-13 09:16:25 +0200

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,9 @@
+//Function för att ändra texten på sidan
 function txt() {
   document.getElementById("txt").innerHTML = "Changed Text!";
 }
 
+//Function för att få en random färg som sedan används i functionen backColor.
 function getRandomColor() {
   var letters = "0123456789ABCDEF".split('');
   var color = "#";
@@ -11,14 +13,17 @@ function getRandomColor() {
   return color;
 }
 
+//Här används random färgen och ändrar backgrunden till den färgen
 function backColor() {
   document.body.style.backgroundColor = getRandomColor();
 }
 
+//Denna function tar användaren till en annan sida
 function newSite() {
   window.location.href = 'HTML/other.html';
 }
 
+//Detta är en anonym function som körs när sidan laddas in och ber användaren mata in sitt namn och sedan välkommnar användaren till sidan
 (function() {
   let person = prompt("Please enter your name", "John Doe");
   if (person === null || person === "") {
