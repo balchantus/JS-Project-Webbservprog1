f6b3771aeff8a3b29067777b419cf45777ac1ee5
balchantus <89977635+balchantus@users.noreply.github.com>
2021-10-19 11:47:37 +0200

    Update other.js

--- JS/other.js
+++ JS/other.js
@@ -1,11 +1,3 @@
-//Detta är en function som rickrollar användaren och öppnar sidan i en ny flik
-function rickRoll() {
-    window.open(
-        'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
-        '_blank'
-    );
-}
-
 //Function som tar användaren tillbaka till startsidan
 function home() {
     window.location.href = '../index.html';
